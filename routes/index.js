5fd87d2e025f3f4ae7d00811bf4d946cd3980168
OUSSAMA AOULI <ousssama.aouli@gmail.com>
2021-04-25 16:54:43 +0000

    enriching facts

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {ensureAuthenticated} = require('../config/auth');
 
 router.get('/', (req, res) => res.render('index',{user: req.user, title:'Home'}));
-router.get('/facts', (req, res) => res.render('facts', {title:'Facts'}));
+router.get('/facts', (req, res) => res.render('facts', {title:'Vaccine Safety Facts'}));
 
 router.get('/dashboard', ensureAuthenticated,(req, res) => res.render('dashboard', {user : req.user, title:'Dashboard'}));
 
