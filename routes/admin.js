d099f75a6f5f930a234a64b4ffedf7e42647e988
OUSSAMA AOULI <ousssama.aouli@gmail.com>
2021-05-23 17:21:03 +0100

    admin dashboard

--- routes/admin.js
+++ routes/admin.js
@@ -7,34 +7,60 @@ const createUser = require('../utilitis/createUser');
 const emailVerification = require("../utilitis/emailVerification");
 
 
-router.get('/addCity/:cityname', ensureAuthenticated,(req,res)=>{
-    console.log(req.user);
-    if(req.params.cityName){
-        if(req.user.AccountType === 'admin'){
-            console.log('yep you are ana admin');
-            City.findOne({name: cityName})
-                .then(city => {
-                    if(city){
-                        console.log('city already exists');
-                    }else {
-                        const newCity = new City({
-                           name: cityname,
-                            director : String,
-                            doctors: [],
-                            patients : [],
-                        });
-                    }
-                })
 
-        }else{
-            res.status(404).render('404',{title: '404 Not Found'});
+async function GetUseIdByCin(cin){
+   return await User.findOne({cin:cin, AccountType: 'doctor'})
+        .then(user => {
+            if(user)
+                return user._id;
+            return null;
+        })
+}
+
+function addCenter(cityId, center) {
+    City.findOne({_id: cityId})
+        .then(theCity =>{
+            theCity.centers.push({
+                centerName: center.centerName,
+                doctors: center.doctors,
+                patients: [],
+            });
+            theCity.save();
+        });
+
+}
+
+
+router.post('/addCity', ensureAuthenticated, (req, res) => {
+    const cityName = req.body.cityName;
+        if (cityName) {
+            if (req.user.AccountType === 'admin') {
+                console.log('yep you are ana admin');
+                City.findOne({name: cityName})
+                    .then(city => {
+                        if (city) {
+                            console.log('city already exists');
+                        } else {
+                            const newCity = new City({
+                                name: cityName.toUpperCase(),
+                                director: String,
+                                doctors: [],
+                                patients: [],
+                            });
+                            newCity.save(function (err) {
+                                console.log(err);
+                            });
+                        }
+                    })
+
+            } else {
+                res.status(404).render('404', {title: '404 Not Found'});
+            }
         }
-    }
-
 });
 
-router.post('/AddDoctor', ensureAuthenticated, (req, res)=>{
-    if(req.user.AccountType === 'admin'){
+router.post('/AddDoctor', ensureAuthenticated, (req, res) => {
+    if (req.user.AccountType === 'admin') {
         const {email, Cin} = req.body;
         const newUser = [
             ['cin', Cin],
@@ -42,7 +68,7 @@ router.post('/AddDoctor', ensureAuthenticated, (req, res)=>{
             ['AccountType', 'doctor'],
         ];
         createUser(newUser, false)
-            .catch(errors=>{
+            .catch(errors => {
                 res.render('dashboard', {
                     errors: errors,
                     title: 'dashboard',
@@ -50,62 +76,101 @@ router.post('/AddDoctor', ensureAuthenticated, (req, res)=>{
                 });
             })
             .then(creation => {
-                    console.log(creation._id);
-                    emailVerification(email, creation._id, 'completeRegistration');
-                    req.flash('success_msg', 'You Created An Doctor Account');
-                    res.redirect('/dashboard');
+                console.log(creation._id);
+                emailVerification(email, creation._id, 'completeRegistration');
+                req.flash('success_msg', 'You Created An Doctor Account');
+                res.redirect('/dashboard');
             })
     }
 });
 
-router.post('/addCenter', ensureAuthenticated, (req, res)=> {
-   if(req.user.AccountType === 'admin'){
-       const {centerName, city, directorCin} = req.body;
-       console.log(directorCin);
-       User.findOne({cin:directorCin.toUpperCase()})
-           .then(user => {
-               if(user){
-                   City.findOne({name: city.toUpperCase(),"centers.centerName":centerName.toUpperCase()})
-                       .then(theCity => {
-                           if(theCity){
-                              theCity.centers.push({
-                                   centerName:centerName.toUpperCase(),
-                                   director : directorCin.toUpperCase(),
-                                   doctors: [],
-                                   patients : [],
-                              });
-                              theCity.save()
-                                  .then(()=>{
-                                      req.flash('success_msg', 'You Added A City');
-                                  })
-                                  .catch(err => {
-                                      req.flash('success_msg', 'Coudnt Add The City');
-                                  });
-                           }else {
-                               const newCity = new City({
-                                   name: city.toUpperCase(),
-                                   centers:[
-                                       {
-                                           centerName:centerName.toUpperCase(),
-                                           director : directorCin.toUpperCase(),
-                                           doctors: [],
-                                           patients : [],
-                                       }
-                                   ]
-                               });
-                               newCity.save()
-                                   .then(()=>{
-                                       console.log('city and center saved');
-                                       req.flash('success_msg', 'You Added A City And A Center');
-                                       res.redirect('/dashboard');
-                                   })
-                                   .catch(err => console.log(err));
-                           }
-                       })
-               }
-           });
+router.post('/addCenter', ensureAuthenticated, async (req, res) => {
+    if (req.user.AccountType === 'admin') {
+        const {centerName, _cityId, doctors} = req.body;
+        City.findOne({_id: _cityId})
+            .then(theCity => {
+                let ex = true;
+                theCity.centers.find(e => e === centerName.toUpperCase());
+                for (let i = 0; i < theCity.centers.length; i++) {
+                    if (theCity.centers[i].centerName === centerName) {
+                        ex = false;
+                        break;
+                    }
+                }
+                if(ex){
+                    theCity.centers.push({
+                        centerName: centerName.toUpperCase(),
+                        doctors: doctors.split(',').map(el => GetUseIdByCin(el)).filter(e => e),
+                        patients: [],
+                    });
+                    theCity.save()
+                        .then(() => {
+                            console.log('center ');
+                            req.flash('success_msg', 'You Added A Center');
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            req.flash('success_msg', 'Could not Add This Center');
+                        });
+                }
+            });
 
-   }
+        // const centerByCity = await City.findOne({"centers.centerName": centerName.toUpperCase()})
+        //     .then(theCity => theCity);
+        // if(centerName)
+
+        // City.findOne({name: city.toUpperCase()})
+        //     //"centers.centerName": centerName.toUpperCase()
+        //     .then(theCity => {
+        //         if (theCity) {
+        //             var ex = null;
+        //             theCity.centers.find(e => e === centerName.toUpperCase());
+        //             for(let i =0; i < theCity.centers.length;i++){
+        //                 if(theCity.centers[i].centerName === centerName){
+        //                     ex = true;
+        //                 }
+        //
+        //             }
+        //             console.log(center);
+        //             // console.log(theCity);
+        //             // theCity.centers.push({
+        //             //     centerName: centerName.toUpperCase(),
+        //             //     doctors: doctors.split(',').map(el => GetUseIdByCin(el)).filter(e => e),
+        //             //     patients: [],
+        //             // });
+        //             // theCity.save()
+        //             //     .then(() => {
+        //             //         console.log('center ');
+        //             //         req.flash('success_msg', 'You Added A Center');
+        //             //     })
+        //             //     .catch(err => {
+        //             //         console.log(err);
+        //             //         req.flash('success_msg', 'Could not Add This Center');
+        //             //     });
+        //         } else {
+        //             console.log(theCity);
+        //             // const newCity = new City({
+        //             //     name: city.toUpperCase(),
+        //             //     centers: [
+        //             //         {
+        //             //             centerName: centerName.toUpperCase(),
+        //             //             doctors: doctors.split(',').map(el => GetUseIdByCin(el)).filter(e => e),
+        //             //             patients: [],
+        //             //         }
+        //             //     ]
+        //             // });
+        //             // newCity.save()
+        //             //     .then(() => {
+        //             //         console.log('city and center saved');
+        //             //         req.flash('success_msg', 'You Added A City And A Center');
+        //             //         res.redirect('/dashboard');
+        //             //     })
+        //             //     .catch(err => console.log(err));
+        //         }
+        //     })
+
+
+    }
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {ensureAuthenticated} = require('../config/auth');
 const pageFieldsByLang = require('../utilitis/lang');
+const City = require('../models/City');
 
 router.get('/', function(req, res){
     res.render('indexL',{user: req.user, title:'Home', pageL:pageFieldsByLang('en','home')})
@@ -14,13 +15,26 @@ router.get('/:lang', function(req, res, next){
         res.render('indexL',{user: req.user, title:'Home', pageL:pageFieldsByLang(req.params.lang, 'home')});
     else next();
 });
+
 router.get('/facts/:lang', (req, res, next) => {
+    console.log(req);
     const lang = req.params.lang;
     if(lang === 'en' || lang === 'fr' || lang === 'ar')
         res.render('factsL', {title:'Vaccine Safety Facts', factsL: pageFieldsByLang(req.params.lang, 'facts')});
     else next();
 });
-router.get('/dashboard', ensureAuthenticated,(req, res) => res.render('dashboard', {user : req.user, title:'Dashboard'}));
+router.get('/dashboard', ensureAuthenticated,(req, res) => {
+    if (req.user.AccountType === 'admin'){
+        City.find({})
+            .then(cities => {
+                res.render('dashboard', {user: req.user, title: 'Dashboard', cities:cities});
+            });
+
+    }
+    else {
+        res.render('dashboard', {user: req.user, title: 'Dashboard'})
+    }
+});
 
 
 module.exports = router;
